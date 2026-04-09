3dbecaf7516828cb4f03505e03b900ce1a1e380a
ifra munawar <codecraftbyifrah@gmail.com>
2026-04-09 20:38:51 +0500

    Updated code

--- oop.py
+++ oop.py
@@ -26,4 +26,34 @@ print(s.name, s.marks)
 ss = Student("ifra", 95)
 print(ss.name, ss.marks)
 print(s.welcome())
+print('..................................................')
 
+# questions
+# Create student class that takes name & marks of 3 subjects as arguments in  constructor.
+# Then create a method to print the average.
+
+class Std:
+
+    def __init__(self, name, sub1, sub2, sub3):
+        self.name = name
+        self.sub1 = sub1
+        self.sub2 = sub2
+        self.sub3 = sub3
+    
+    def average(self):
+        avg = (self.sub1 + self.sub2 + self.sub3)/3
+        print(f"The average marks of {self.name} is: {avg:.2f}")
+
+    @staticmethod                    # static method function used to avoid self parameter
+    def conclusion():
+        print( 'its endinggggg ')
+
+c1 = Std("alia", 66, 84, 98)
+c2 = Std("saba", 87, 54, 45)
+c3 = Std("zuha", 56, 83, 67)
+
+# c1.name = 'ifra'
+c1.average()
+c2.average()
+c3.average()
+c3.conclusion()
