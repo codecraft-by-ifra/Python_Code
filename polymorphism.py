42b71d27796362f96743f80f2bfc770429f027d0
ifra munawar <codecraftbyifrah@gmail.com>
2026-04-10 17:21:53 +0500

    Updated code

---
+++ polymorphism.py
@@ -0,0 +1,46 @@
+# Dunder/ magic  methods
+class Complex:
+
+    def __init__(self, real , img ):
+        self.real = real
+        self.img = img
+
+    def addnumb(self):
+        print(f'{self.real}i + {self.img}j')
+
+    def subnumb(self):
+        print(f'{self.real}i - {self.img}j')
+
+# use underscore in end or start for dunder method
+    def __add__(self, num2):                  # self = num1
+        Nreal = self.real + num2.real
+        Nimg = self.img + num2.img
+        return Complex(Nreal, Nimg)
+    
+    def __sub__(self, num2):                  # self = num1
+        Nreal = self.real - num2.real
+        Nimg = self.img - num2.img
+        return Complex(Nreal, Nimg)
+    
+
+num1= Complex(1,3)
+num2= Complex(5,2)
+num3 = num1 + num2
+num4 = num1 - num2
+
+# for addition printing 
+num1.addnumb()
+num2.addnumb()
+print('--------')
+num3.addnumb()
+print('--------')
+print('--------')
+
+
+# for subtraction printing
+num1.subnumb()
+num2.subnumb()
+print('--------')
+num4.subnumb()
+print('--------')
+print('--------')
