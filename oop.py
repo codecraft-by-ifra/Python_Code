c9b162e6ba12314860bc3310a98be6af4313f02a
ifra munawar <codecraftbyifrah@gmail.com>
2026-04-10 16:40:09 +0500

    Updated code

--- oop.py
+++ oop.py
@@ -1,11 +1,20 @@
 class Home:                      # class
     color = "white"
     location = "kasur"
+    comp = 'azire'
+
+    @classmethod          # for chnging the value permanently
+    def changename(cls, comp):
+        cls.comp = comp
 
 room = Home()                    # object
 hall = Home()
 print(room.color)
 print(hall.location)
+
+print(f'first name of company is : {room.comp}')
+room.changename("cringe")
+print(f'modified of comapny is : {room.comp}')
 
 
 # Constructor
@@ -40,7 +49,6 @@ print('..................................................')
 # Then create a method to print the average.
 
 class Std:
-    comp = 'azire'
 
     def __init__(self, name, sub1, sub2, sub3):
         self.name = name
@@ -56,9 +64,7 @@ class Std:
     def conclusion():
         print( 'its endinggggg ')
 
-    @classmethod          # for chnging the value permanently
-    def changename(cls, comp):
-        cls.comp = comp
+   
 
 c1 = Std("alia", 66, 84, 98)
 c2 = Std("saba", 87, 54, 45)
@@ -69,8 +75,25 @@ c1.average()
 c2.average()
 c3.average()
 c3.conclusion()
-print(c1.comp)
-c1.changename("cringe")
-print(c1.comp)
 
 
+
+# property decorator
+
+class Books:
+    def __init__(self, bio, sst, geo):
+        self.bio = bio
+        self.sst = sst
+        self.geo = geo
+    
+    @property
+    def percentage(self):
+        prcnt = (self.bio + self.sst + self.geo)/3
+        return str(f'Percentage is {prcnt:.2f}')
+    
+bk1 = Books(98,97,99)
+print(bk1.percentage)
+
+bk1.sst = 86
+
+print(bk1.percentage)
